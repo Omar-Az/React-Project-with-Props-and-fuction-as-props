6840082ffc2416ae77cb863d4d55f60d7d0e5bf2
Omar-Az <omaraziz443@gmail.com>
2021-08-27 14:41:03 +0500

    First commit

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className='navbar'>
+            <h1>Omar's First App</h1>
+            <div className='links'>
+                <a href="/">Home</a> 
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar
